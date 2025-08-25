6e712416dd93637bc82201ebe983d7cb7a5d8f72
peterakintonde5-maker <peterakintonde5@gmail.com>
2025-08-25 23:11:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     // ===== BOT CORE SETTINGS =====
 
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~8ZxQzKJQ#GDS6LPYV3rNh75iaXdHjrRu10ZIjoJCA5Ys0g9xKAkk",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~NcBQUZiB#OyIen1aVERqj2KLavL_AFNw9WzH-2GKIOIOi0IzTR8E",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
